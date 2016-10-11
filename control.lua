5bdc918bbedc0bba30b9a246bac3d09fc06d9d47
Ps7cho <chocolatetthunder@gmail.com>
2016-10-11 15:11:56 -0400

    added server messeges
    prints to consol for the gui on join and on left so we can see from the
    web who they are

--- control.lua
+++ control.lua
@@ -576,6 +576,34 @@ function starting_inventory(event)
 	player.insert{name="fish", count = 10}
 end
 
+function player_died(event)
+  game.speed = 1
+  player = event.player_index
+  if game.players[player].name ~= nil then
+    print("[PUPDATE] | "..game.players[player].name.." | died |"..game.players[player].force.name)
+  end
+end
+
+function player_joined(event)
+  game.speed = 1
+  player = event.player_index
+  if game.players[player].name ~= nil then
+    print("[PUPDATE] | "..game.players[player].name.." | join")
+  end
+end
+
+
+function player_left(event)
+  player = event.player_index
+  if game.players[player].name ~= nil then
+    print("[PUPDATE] | "..game.players[player].name.." | leave")
+  end
+end
+
+Event.register(defines.events.on_player_joined_game, on_player_died)
+Event.register(defines.events.on_player_joined_game, player_joined)
+Event.register(defines.events.on_player_left_game, player_left)
+
 function show_health()
     for k, player in pairs(game.players) do
 		if player.connected then
