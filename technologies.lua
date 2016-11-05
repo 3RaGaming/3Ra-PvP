a0b6b951b1d7a5d250fafeec1f8413c7c7c40582
Ps7cho <chocolatetthunder@gmail.com>
2016-11-05 15:14:02 -0400

    research revert
    changed researching all technologies to prolong and balnace gameplay
    changed how disableing recipies are handled.

--- technologies.lua
+++ technologies.lua
@@ -2,14 +2,25 @@
 function research_technology()
 	for k, force in pairs (game.forces) do
 	  for k, technology in pairs (force.technologies) do 
-		technology.researched = true
+		technology.researched = false
 			if technology.upgrade then
 			technology.researched = false
 			end
 		end
-		force.recipes["energy-shield-mk2-equipment"].enabled=false
-		force.recipes["laser-turret"].enabled=false
-		force.recipes["destroyer-capsule"].enabled=false
-		force.recipes["personal-laser-defense-equipment"].enabled=false
 	end	
 end
+
+script.on_event(defines.events.on_research_finished, function (event)
+
+  local research = event.research
+
+
+  research.force.recipes["laser-turret"].enabled=false
+  research.force.recipes["discharge-defense-equipment"].enabled=false
+  research.force.recipes["construction-robot"].enabled=false
+  research.force.recipes["discharge-defense-remote"].enabled=false
+  research.force.recipes["energy-shield-mk2-equipment"].enabled=false
+  research.force.recipes["personal-laser-defense-equipment"].enabled=false
+  research.force.recipes["destroyer-capsule"].enabled=false
+
+end)
