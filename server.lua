9782ed662e0f40c0a4d323233d48fcf87acc6879
zackman0010 <computerboyzack@gmail.com>
2016-11-13 23:16:47 -0600

    Updated for New Webserver Program
    Added some lines that can be parsed by the new webserver management
    program

--- server.lua
+++ server.lua
@@ -1,29 +1,31 @@
 function player_died(event)
-  player = event.player_index
-  if game.players[player].name ~= nil then
-    print("[PUPDATE] | "..game.players[player].name.." | died |"..game.players[player].force.name)
-  end
+	local player = event.player_index
+	if game.players[player].name ~= nil then
+		print("[PUPDATE] | "..game.players[player].name.." | died | "..game.players[player].force.name)
+	end
 end
 
 function player_respawned(event)
-  player = event.player_index
-  if game.players[player].name ~= nil then
-    print("[PUPDATE]| "..game.players[player].name.." | respawn |"..game.players[player].force.name)
-  end
+	local player = event.player_index
+	if game.players[player].name ~= nil then
+		print("[PUPDATE]| "..game.players[player].name.." | respawn | "..game.players[player].force.name)
+	end
 end
 
 function player_joined(event)
-  player = event.player_index
-  if game.players[player].name ~= nil then
-    print("[PUPDATE]| "..game.players[player].name.." | join |"..game.players[player].force.name)
-  end
+	local player = event.player_index
+	if game.players[player].name ~= nil then
+		print("[PUPDATE]| "..game.players[player].name.." | join | "..game.players[player].force.name) -- Print for human readability
+		print("PLAYER$join," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name) -- Print for computer parsing
+	end
 end
 
 function player_left(event)
-  player = event.player_index
-  if game.players[player].name ~= nil then
-    print("[PUPDATE]| "..game.players[player].name.." | leave |"..game.players[player].force.name)
-  end
+	local player = event.player_index
+	if game.players[player].name ~= nil then
+		print("[PUPDATE]| "..game.players[player].name.." | leave | "..game.players[player].force.name) -- Print for human readability
+		print("PLAYER$leave," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name) -- Print for computer parsing
+	end
 end
 
 Event.register(defines.events.on_player_died, player_died)
